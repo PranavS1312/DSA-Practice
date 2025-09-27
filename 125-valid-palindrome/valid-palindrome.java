d6af90c9a3d19a1405a25e56d58d218795d8faa9
Pranav Shinde <144710408+PranavS1312@users.noreply.github.com>
2025-09-27 21:47:39 +0530

    Time: 12 ms (42.41%) | Memory: 45.2 MB (18.74%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+//using stringbuilder
+        String a = s.trim().replaceAll("\\s+","").toLowerCase();
+        StringBuilder b = new StringBuilder();
+
+        for(int i=0;i<a.length();i++){
+            
+            if((a.charAt(i)>96 && a.charAt(i)<123) || (a.charAt(i)>47 && a.charAt(i)<58)){
+                b.append(a.charAt(i));
+            }
+        }
+       
+        StringBuilder rev = new StringBuilder(b).reverse();
+
+        return b.toString().equals(rev.toString());
+        
+    }
+}
